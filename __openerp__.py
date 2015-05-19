452fd8f595569f42c82b9a62c44d91e3efe2cf39
Daniel Blanco <daniel@blancomartin.cl>
2015-05-19 12:54:20 -0300

    First commit for forked product_image_filestore from Ivan Yelizariev

---
+++ __openerp__.py
@@ -0,0 +1,11 @@
+{
+    'name' : 'Store product images in filestore',
+    'version' : '1.0.0',
+    'author' : 'Ivan Yelizariev',
+    'category' : 'Custom',
+    'website' : 'https://yelizariev.github.io',
+    'summary': 'Check WARNING before installing \ uninstalling!',
+    'depends' : ['product'],
+    'data':[],
+    'installable': True
+}
